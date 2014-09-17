ab1f3b1fbbbc0bd3301c61c8052253104c874183
Ronald <dada5714@yahoo.com>
2014-09-17 10:57:49 -0700

    Fix Spec Helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,2 @@
 require 'minitest/autorun'
 require 'minitest/spec'
-
-# Class FeatureSpec
-class FeatureSpec < MiniTest::Spec
-  require 'capybara/poltergeist'
-  include Capybara::DSL
-  Capybara.default_driver = :poltergeist
-  register_spec_type(/page$/, self)
-end
